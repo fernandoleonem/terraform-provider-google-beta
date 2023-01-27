6d4800841d76e5e7ec402582fca898b89aed232d
afpesseti <afpesseti@ciandt.com>
2023-01-27 13:07:50 -0300

    fix: test

--- google-beta/resource_compute_network_edge_security_services_test.go
+++ google-beta/resource_compute_network_edge_security_services_test.go
@@ -29,45 +29,11 @@ func TestAccComputeNetworkEdgeSecurityServices_basic(t *testing.T) {
 	})
 }
 
-func TestAccComputeNetworkEdgeSecurityServices_basicTwo(t *testing.T) {
-	t.Parallel()
-
-	spName := fmt.Sprintf("tf-test-%s", randString(t, 10))
-
-	vcrTest(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckComputeSecurityPolicyDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccComputeNetworkEdgeSecurityServices_basicTwo(spName),
-			},
-			{
-				ResourceName:      "google_compute_network_edge_security_services.services",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
-
 func testAccComputeNetworkEdgeSecurityServices_basic(spName string) string {
 	return fmt.Sprintf(`
 resource "google_compute_network_edge_security_services" "services" {
   name        = "%s"
   description = "basic network edge security services"
-  region = "southamerica-east1-b"
-}
-`, spName)
-}
-
-func testAccComputeNetworkEdgeSecurityServices_basicTwo(spName string) string {
-	return fmt.Sprintf(`
-resource "google_compute_network_edge_security_services" "services" {
-  name        = "%s"
-  description = "basic network edge security services"
-  region = "southamerica-east1-c"
 }
 `, spName)
 }
