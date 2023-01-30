814cb95c6b1bc3c8af9ea5013789de0f76208fdf
afpesseti <afpesseti@ciandt.com>
2023-01-30 12:12:28 -0300

    fix

--- google-beta/resource_compute_security_policy_test.go
+++ google-beta/resource_compute_security_policy_test.go
@@ -31,28 +31,6 @@ func TestAccComputeSecurityPolicy_basic(t *testing.T) {
 	})
 }
 
-func TestAccComputeSecurityPolicy_basic_withEdge(t *testing.T) {
-	t.Parallel()
-
-	spName := fmt.Sprintf("tf-test-%s", randString(t, 10))
-
-	vcrTest(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckComputeSecurityPolicyDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccComputeSecurityPolicy_basic_withNetwork(spName),
-			},
-			{
-				ResourceName:      "google_compute_security_policy.policy",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
 func TestAccComputeSecurityPolicy_withRule(t *testing.T) {
 	t.Parallel()
 
@@ -497,17 +475,6 @@ resource "google_compute_security_policy" "policy" {
   name        = "%s"
   description = "basic security policy"
   type        = "CLOUD_ARMOR"
-}
-`, spName)
-}
-
-func testAccComputeSecurityPolicy_basic_withNetwork(spName string) string {
-	return fmt.Sprintf(`
-resource "google_compute_security_policy" "policy" {
-  name        = "%s"
-  description = "basic security policy"
-  type        = "CLOUD_ARMOR_NETWORK"
-  region = "us-central1"
 }
 `, spName)
 }
