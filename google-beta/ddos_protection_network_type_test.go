5c7878aeb38605ad4dd121a39b56c1c2ce39f9a0
afpesseti <afpesseti@ciandt.com>
2023-01-30 11:29:56 -0300

    fix: test

--- google-beta/ddos_protection_network_type_test.go
+++ google-beta/ddos_protection_network_type_test.go
@@ -7,29 +7,6 @@ import (
 )
 
 //ChangeTest
-func TestAccComputeSecurityPolicy_basicWithCloudArmorNetworkType(t *testing.T) {
-	t.Parallel()
-
-	spName := fmt.Sprintf("tf-test-%s", randString(t, 10))
-
-	vcrTest(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckComputeSecurityPolicyDestroyProducer(t),
-		Steps: []resource.TestStep{
-			{
-				Config: testAccComputeSecurityPolicy_basicWithCloudArmorNetworkType(spName),
-			},
-			{
-				ResourceName:      "google_compute_security_policy.policy",
-				ImportState:       true,
-				ImportStateVerify: true,
-			},
-		},
-	})
-}
-
-//ChangeTest
 func TestAccComputeSecurityPolicy_withDdosProtectionConfig(t *testing.T) {
 	t.Parallel()
 
@@ -51,18 +28,6 @@ func TestAccComputeSecurityPolicy_withDdosProtectionConfig(t *testing.T) {
 		},
 	})	
 }
-
-//ChangeMock
-func testAccComputeSecurityPolicy_basicWithCloudArmorNetworkType(spName string) string {
-	return fmt.Sprintf(`
-resource "google_compute_security_policy" "policy" {
-  name        = "%s"
-  description = "basic security policy"
-  type        = "CLOUD_ARMOR_NETWORK"
-}
-`, spName)
-}
-
 
 //ChangeMock
 func testAccComputeSecurityPolicy_withDdosProtectionConfig(spName string) string {
