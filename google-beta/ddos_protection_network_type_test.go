8c0a48bee663415b80e8e9156505092b4691463c
afpesseti <afpesseti@ciandt.com>
2023-01-30 12:16:23 -0300

    fix

--- google-beta/ddos_protection_network_type_test.go
+++ google-beta/ddos_protection_network_type_test.go
@@ -60,6 +60,13 @@ resource "google_compute_security_policy" "policy" {
   rule {
     action   = "deny-502"
     priority = "2147483647"
+	
+	match {
+		versioned_expr = "SRC_IPS_V1"
+		config {
+		  src_ip_ranges = ["*"]
+		}
+	  }
   }
 
   rule {
