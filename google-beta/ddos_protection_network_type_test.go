c80b18669b463e321510a161b66f7ae93ad5503c
afpesseti <afpesseti@ciandt.com>
2023-01-30 21:25:02 -0300

    fix

--- google-beta/ddos_protection_network_type_test.go
+++ google-beta/ddos_protection_network_type_test.go
@@ -79,6 +79,7 @@ resource "google_compute_security_policy" "policy" {
 		  src_ip_ranges = ["192.0.2.0/24"]
 		}
 		expr {
+			expression = "US"
 			location = "us-central1"
 		}
 	  }
@@ -105,6 +106,7 @@ resource "google_compute_security_policy" "policy" {
         src_ip_ranges = ["*"]
       }
 	  expr {
+		expression = "US"
 		location = "us-central1"
 	}
 	  
